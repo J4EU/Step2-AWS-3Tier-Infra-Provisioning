431c634a5f1a6af84e80434c16c001c95e9fc84d
JxxHyk <choiieoe@naver.com>
2026-04-11 14:52:38 +0900

    feat: WAS 서버 EC2 인스턴스 추가

---
+++ was.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "guestbook_was_server" {
+  ami           = data.aws_ami.al2023.id
+  instance_type = "t3.mirco"
+  key_name      = "guestbook-was"
+
+  subnet_id = aws_subnet.guestbook_private_sn.id
+
+  vpc_security_group_ids = [aws_security_group.guestbook_was_sg]
+
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "guestbook-was-server"
+  }
+}
