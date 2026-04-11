e617c63ac4798a5b1892c92217a7d693119ddc9c
JxxHyk <choiieoe@naver.com>
2026-04-11 15:00:03 +0900

    fix: 인스턴스 타입 오타 수정

--- was.tf
+++ was.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "guestbook_was_server" {
   ami           = data.aws_ami.al2023.id
-  instance_type = "t3.mirco"
+  instance_type = "t3.micro"
   key_name      = "guestbook-was"
 
   subnet_id = aws_subnet.guestbook_private_sn.id
 
-  vpc_security_group_ids = [aws_security_group.guestbook_was_sg]
+  vpc_security_group_ids = [aws_security_group.guestbook_was_sg.id]
 
   associate_public_ip_address = false
 
