7a0c916e24b0f0a8c9cce30a587bbb7bea66be24
JxxHyk <choiieoe@naver.com>
2026-04-11 14:37:10 +0900

    feat: MariaDB 설치 및 시작 스크립트 추가

--- db.tf
+++ db.tf
@@ -6,6 +6,13 @@ resource "aws_instance" "guestbook_db_server" {
   subnet_id              = aws_subnet.guestbook_private_sn.id
   vpc_security_group_ids = [aws_security_group.guestbook_db_sg.id]
 
+  user_data = <<-EOF
+        #!/bin/bash
+        dnf update -y
+        dnf install -y mariadb-server
+        systemctl enable --now mariadb
+        EOF
+
   tags = {
     Name = "guestbook-db-server"
   }
