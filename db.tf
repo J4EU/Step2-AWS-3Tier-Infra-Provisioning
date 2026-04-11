31790559cf74e98f1c321f12380f50c6e1e7be24
JxxHyk <choiieoe@naver.com>
2026-04-11 14:49:08 +0900

    fix: key_name 오타 수정 및 공용 IP 할당 비활성화

--- db.tf
+++ db.tf
@@ -1,10 +1,12 @@
 resource "aws_instance" "guestbook_db_server" {
   ami           = data.aws_ami.al2023.id
   instance_type = "t3.micro"
-  key_name      = "guestbook-web"
+  key_name      = "guestbook-db"
 
   subnet_id              = aws_subnet.guestbook_private_sn.id
   vpc_security_group_ids = [aws_security_group.guestbook_db_sg.id]
+
+  associate_public_ip_address = false
 
   user_data = <<-EOF
         #!/bin/bash
